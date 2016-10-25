704b7ab9ebb984531c65dbacd3e45d0d563eee2e
zionaranda <zaranda@udel.edu>
2016-10-25 18:59:04 -0400

    Removed Constructors
     From Oracle: "Abstract classes cannot be instantiated, but they can be subclassed."
    --I think this means abstract classes should not have constructors.

--- ACharacter.java
+++ ACharacter.java
@@ -3,18 +3,6 @@ public abstract class ACharacter {
 	int xpos,ypos;
 	
 	/**
-	 * Default constructor for a character with no arguments
-	 */
-	ACharacter(){}
-	
-	/**
-	 * Constructor for a character with x and y given
-	 * @param startx starting x coordinate
-	 * @param starty starting y coordinate
-	 */
-	ACharacter(int startx, int starty){}
-	
-	/**
 	 * Moves a character given an x direction and y direction
 	 * @param xdir x direction the character is moving
 	 * @param ydir y direction the character is moving
