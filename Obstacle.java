201ddcc50728e848846f13c1f4ce1e2ec3a5b9ad
kblack789 <kblacks@udel.edu>
2016-10-25 19:33:28 -0400

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -1,19 +1,16 @@
 abstract class Obstacle {
 	bool beenTreated;
 
-	void removeItem() {
+	void removeItem(); 
 		/* remove ACollectableObject from view, delete object */
-	}
-
-	abstract bool checkItem(Object o1){
+	
+	abstract bool checkItem(Object o1);
 		/* Determines if item dropped on self is correct */
 	}
 
-	abstract void damageChar(){
+	abstract void damageChar();
 		/* Reduce health meter */
-	}
 
-	abstract void receiveItem(){
+	abstract void receiveItem();
 		/* Modifies damage values and */
-	}
 }
