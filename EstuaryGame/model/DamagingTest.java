331bc311873c318cb642969d44b071e58c90fab8
kblack789 <kblacks@udel.edu>
2016-10-26 17:43:43 -0400

    Add files via upload

---
+++ EstuaryGame/model/DamagingTest.java
@@ -0,0 +1,16 @@
+package model;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DamagingTest {
+
+	@Test
+	public void damageTest() {
+		
+	}
+
+	@Test
+	public void checkBarrierTest(){
+		
+	}
+}
