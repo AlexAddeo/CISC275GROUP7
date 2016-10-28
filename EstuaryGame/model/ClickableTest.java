9982526f04c8f659294d598bd2f9fca53b9276e4
kblack789 <kblacks@udel.edu>
2016-10-27 20:23:56 -0400

    Update ClickableTest.java

--- EstuaryGame/model/ClickableTest.java
+++ EstuaryGame/model/ClickableTest.java
@@ -5,8 +5,18 @@ import org.junit.Test;
 public class ClickableTest {
 
 	@Test
-	public void removeClickableTest() {
-		CollectableObjcet clickableP = this;
-		this.removeFromScreen();
+	public void removeFromScreenTest() {
+		ClickableObject clickableP = new ClickableObject();
+		clickableP.removeFromScreen();
 		assertEquals("Clickable Pointer should be null when removed", null, clickableP);
 	}
+	
+	public void addToCount(){
+		ClickableObject clickableP = new ClickableObject();
+		clickableObject.count = 10;
+		int curr = clickableObject.count;
+		clickableP.addToCount();
+		assertEquals("Count should be increased by 1.", curr+1, clickableObject.count);
+	}
+	
+}
