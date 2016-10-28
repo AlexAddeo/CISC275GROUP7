7b8dc627e40fb65db8e95567957ee832f3205a9b
AlexAddeo <alexaddeo@hotmail.com>
2016-10-27 21:30:03 -0400

    Final RunoffTest commit
    heyo

--- RunoffTest.java
+++ RunoffTest.java
@@ -5,34 +5,21 @@ import org.junit.Test;
 public class RunoffTest extends Runoff{
 
 	//want to test the methods within runoff.
-	private Object obj;
 	
-	public RunoffTest(Object o1){
-		
-		this.obj = o1;
-	}
+	PlaceableObject placeable = new PlaceableObject();
 	
-	@Test
-	public void checkBarrierTest(){
-		//need to check and see what type of object is being passed in.
-		//if a gabion, need to use gabion damage. 
-		//if a plant, need to use plant damage.
-		//if the runoff reaches water, need to use water damage.
-		assertSame("object is a gabion.", new Gabion(), this.obj);
-		assertSame("object is a plant.", new Plant(), this.obj);
-		assertSame("object is water.", new Water(), this.obj);
-	}
-	
+	Runoff run = new Runoff();
+
 	@Test
 	public void damageTest(){
 		//Testing to see if damage works.
 		//Damage will be different after checking the barrier. Can't write tests for this
 		// until the code in the methods is completed.
-		int expected_damage = 20;
+		int expected_damage = placeable.health-1;
+
+		run.damage(placeable);
 		
-		Runoff chemicals = new Runoff();
-		chemicals.damage(this.obj);
-		assertEquals("20 points of damage should be given.", expected_damage, ((PlaceableObject)obj).health);
+		assertEquals("1 point of damage should be given.", expected_damage, placeable.health);
 	}
 	
 }
