4cb6f54dde6ac37b6445b352b356b14969df8a0e
michaelcisternino <mcistern@udel.edu>
2016-10-27 22:08:50 -0400

    Update FallingObject.java

--- EstuaryGame/model/FallingObject.java
+++ EstuaryGame/model/FallingObject.java
@@ -1,17 +1,43 @@
 package model;
 
-public abstract class FallingObject extends InteractiveObject{
+public class FallingObject extends InteractiveObject{
 
-	private boolean falling = true;
-	private boolean caught = false;
-	private boolean bottom = false;
+	public boolean falling = true;
+	public boolean caught = false;
+	public boolean bottom = false;
+	public final int FALL_SPEED = 5;
+	
+	private FallType type;
+	
+	
+	public FallingObject(FallType f){
+		this.type = f;
+		this.ypos = 400;
+	}
 	
 	public void fall(){
-		
+		this.ypos -= 5;
+		if(this.ypos <= 0){
+			bottom = true;
+		}
 	};
 	
 	public void stop(){
-		falling = false;
+		if(caught = true){
+			falling = false;
+		}
+		else if(bottom = true){
+			if(this.getType() == FallType.Fish){
+				falling = true;
+			}
+			else{
+			falling = false;
+			}
+		}
 	};
+	
+	public FallType getType(){
+		return this.type;
+	}
 	
 }
